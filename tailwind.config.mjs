242790644017c0f81966a7e8a26dc52e5ce6f8c8
rsganesh <rshivaganesh@gmail.com>
2025-06-12 09:26:16 +0530

    tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+import aspectRatio from '@tailwindcss/aspect-ratio';
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
+    typography,
+    aspectRatio,
   ],
 } 
