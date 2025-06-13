2fc176b8b592bc83c4f82f9d4279c4fd63ec0d95
rsganesh <rshivaganesh@gmail.com>
2025-06-13 15:01:11 +0530

    updated code

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: netlify()
+  output: 'static',
+  adapter: netlify(),
+  build: {
+    format: 'directory'
+  }
 });
